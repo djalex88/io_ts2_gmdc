87fb75f27716895c84b50da8f9f047ee29b5e1cc
DjAlex88 <d30e5c@gmail.com>
2021-05-01 04:05:07 +0200

    Updated gmdc_tools/_common.py. Removed encode/decode for Python 2. And some minor changes.

--- gmdc_tools/_common.py
+++ gmdc_tools/_common.py
@@ -31,14 +31,52 @@ from struct import pack, unpack
 if sys.version_info[0] == 3:
 	# Python 3
 	import builtins
+
 	xrange = range
+
 	def map(func, *iterables):
 		return list(builtins.map(func, *iterables))
+
 	def filter(func, iterable):
 		return list(builtins.filter(func, iterable))
+
 	def zip(*iterables):
 		return list(builtins.zip(*iterables))
+
 	__all__+= ['xrange', 'map', 'filter', 'zip']
+
+	# files are read in binary mode ('rb')
+	# in Python 3 this gives raw byte array
+	def read_str(f):
+		i = unpack('B', f.read(1))[0]
+		s = f.read(i).decode('latin_1')
+		return s
+
+	def write_str(f, s):
+		s = s.encode('latin_1')
+		f.write(pack('B', len(s)) + s)
+
+	def to_hex(s):
+		return '\x20'.join('%02X'%x for x in s)
+
+else:
+	# in Python 2 str == bytes
+	# i.e., no need to encode / decode
+	def read_str(f):
+		i = unpack('B', f.read(1))[0]
+		s = f.read(i)
+		return s
+
+	def write_str(f, s):
+		f.write(pack('B', len(s)) + s)
+
+	def to_hex(s):
+		return '\x20'.join('%02X'%ord(x) for x in s)
+
+
+# logging
+
+log_file = None
 
 def set_log_file(f):
 	global log_file
@@ -47,21 +85,23 @@ def set_log_file(f):
 def close_log_file():
 	global log_file
 	if log_file != None:
-		log_file.close() ; log_file = None
+		log_file.close()
+		log_file = None
 
 def log(*args):
 	s = '\x20'.join(str(x) for x in args)
 	print(s)
-	if log_file != None: print(s, file=log_file)
+	if log_file != None:
+		print(s, file=log_file)
 
 def error(*args):
 	s = '\x20'.join(str(x) for x in args)
 	print(s, file=sys.stderr)
-	if log_file != None: print(s, file=log_file)
+	if log_file != None:
+		print(s, file=log_file)
 
-def to_hex(s):
-	if isinstance(s, bytes): s = s.decode('latin_1')
-	return '\x20'.join('%02X'%ord(x) for x in s)
+
+# other
 
 def chunk(seq, sublen):
 	return [seq[i:i+sublen] for i in xrange(0, len(seq), sublen)]
@@ -73,14 +113,6 @@ def print_last_exception():
 	while tb:
 		c = tb.tb_frame.f_code
 		error( '\x20\x20'*i + '--Function: "%s" in "%s", line: %i' % (c.co_name, c.co_filename, tb.tb_lineno) )
-		tb = tb.tb_next ; i+= 1
+		tb = tb.tb_next
+		i = i + 1
 
-def read_str(f):
-	i = unpack('B', f.read(1))[0]
-	return f.read(i).decode('latin_1')
-
-def write_str(f, s):
-	s = s.encode('latin_1')
-	f.write(pack('B', len(s)) + s)
-
-log_file = None
